f1751f9340a991dc7a583f9445dcb4af411992eb
Rodrigo Iglesias <rodrigo.iglesias@bigbox.com.ar>
2024-12-03 22:42:29 +0000

    Updaten about

--- src/components/About.js
+++ src/components/About.js
@@ -8,14 +8,28 @@ const About = () => {
       job: "Redactor",
       img: "https://placehold.co/350x400/0079fa/EEE?font=roboto&text=foto%20de%20tristan",
       linkedin: "https://www.linkedin.com",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel dolor in elit porttitor commodo. Sed feugiat, nisl at viverra consectetur, augue odio fringilla lacus, quis lacinia urna metus nec quam. Donec ut massa non nunc cursus fermentum.",
+      description: `
+        Siempre creí que las palabras tienen un poder único. No solo cuentan historias; conectan, despiertan ideas y, a veces, hasta cambian algo en quien las escucha. Soy Tristán, alguien que disfruta explorarlas, dándoles forma hasta encontrar esa chispa que las hace memorables.
+
+        No me considero ningún poeta, sino un hábil curioso. Cada proyecto me lleva a encontrar lo que hace que una idea diga algo más. Como decía Spinetta, “el talento es el hombre en libertad, nace en cualquier persona que se sienta capaz de volar con sus ideas”.
+
+        — Periodista para el Diario Olé 2023 - 2024
+        — Content Writer para Jampp 2024 - actualidad
+      `,
     },
     {
       name: "Julian Di Tullio",
-      job: "Backend Developer",
+      job: "Director de arte",
       img: "https://placehold.co/350x400/0079fa/EEE?font=roboto&text=foto%20de%20julian",
       linkedin: "https://www.linkedin.com",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel dolor in elit porttitor commodo. Sed feugiat, nisl at viverra consectetur, augue odio fringilla lacus, quis lacinia urna metus nec quam. Donec ut massa non nunc cursus fermentum.",
+      description: `
+        Para mí, cada proyecto es un espacio de experimentación, donde las ideas toman forma gracias a la combinación justa de creatividad, técnica y una mezcla de referencia recolectadas a lo largo de mis 24 años. Mi nombre es Julian, una persona curiosa que siempre esta en la búsqueda de nuevas herramientas y tendencias.
+
+        No se trata solo de hacer cosas lindas, sino transmitir un mensaje y darle un valor agregado a eso.
+
+        — Diseñador freelance 2020-2023
+        — Diseñador multimedia para T3H 2023-actualidad
+      `,
     },
   ]
 
@@ -23,12 +37,7 @@ const About = () => {
     <>
       <section className="about wrapper">
         <SectionTitle title="tristán&julian" />
-        <p className="about-description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel
-          dolor in elit porttitor commodo. Sed feugiat, nisl at viverra
-          consectetur, augue odio fringilla lacus, quis lacinia urna metus nec
-          quam. Donec ut massa non nunc cursus fermentum.
-        </p>
+        <p className="about-description"></p>
         <section className="persons">
           <p className="about-sub">(MEET US)</p>
           {persons.map((person, index) => (
